b01c3e2ec3bcbd2c9b9290c270e336772929ef8d
unknown <ezequiel.buzzella@gmail.com>
2022-10-11 08:43:33 -0300

    Agregado interfaz y unas imagenes

---
+++ src/app/books-list/Book.ts
@@ -0,0 +1,6 @@
+import { Type } from "@angular/compiler";
+
+export interface Book{
+    type:string,
+    images:string
+}
--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from './Book';
 
 @Component({
   selector: 'app-books-list',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books-list.component.scss']
 })
 export class BooksListComponent implements OnInit {
-
+  books:Book[]=[
+    {
+    type:"horror",
+    images:"assets/images/Horror/images.jpg",
+  },
+  {
+    type:"horror",
+    images:"assets/images/Horror/tales_horror.jpg"
+  },
+  {
+    type:"horror",
+    images:"assets/images/Horror/images (1).jpg"
+  }
+  ]
   constructor() { }
 
   ngOnInit(): void {

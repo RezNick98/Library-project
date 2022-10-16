4e0e6feb8659950962a4033ca0e9363335ff1482
RezNick98 <ezequiel.buzzella@gmail.com>
2022-10-16 18:17:02 -0300

    Arreglado ciertos problemas con css

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -12,25 +12,25 @@ export class BooksListComponent implements OnInit {
       name:"Pet Sementary",
     type:"horror",
     images:"assets/images/Horror/images.jpg",
-    available:true
+    available:true,
   },
   {
     name:"Tales Horror",
     type:"horror",
     images:"assets/images/Horror/tales_horror.jpg",
-    available:true
+    available:true,
   },
   {
     name:"The Outsider",
     type:"horror",
     images:"assets/images/Horror/images (1).jpg",
-    available:true
+    available:true,
   },
   {
   name:"The Tokyo Zodiacs Murders",
   type:"horror",
   images:"assets/images/Horror/TheTokyoZOdiacMurders.jpg",
-  available:false
+  available:false,
   }
   
 ]
@@ -39,27 +39,26 @@ export class BooksListComponent implements OnInit {
     name:"Journey To The Center Of The Earth",
     type:"adventure",
     images:"assets/images/Fantasy/journey.jpg",
-    available:true
+    available:true,
   },
   {
     name:"Moby Dick",
     type:"fantasy",
     images:"assets/images/Fantasy/moby.jpg",
-    available:true
+    available:true,
   },
   {
     name:"Jurassic Park",
     type:"fantasy",
     images:"assets/images/Fantasy/jp.jpg",
-    available:true
+    available:true,
   },
   {
     name:"Shadows On The Moon",
     type:"fantasy",
     images:"assets/images/Fantasy/Shadows-on-the-Moon.jpeg",
-    available:false
+    available:false,
   }
-
 ]
   constructor() { }
 

c0bd965986f770e4d9f6f61d296b64947dc8412e
RezNick98 <ezequiel.buzzella@gmail.com>
2022-10-16 18:04:08 -0300

    Hecho directivas componentes y agregado estilo a warnings

--- src/app/books-list/Book.ts
+++ src/app/books-list/Book.ts
@@ -1,6 +1,8 @@
 import { Type } from "@angular/compiler";
 
 export interface Book{
+    name:String
     type:string,
     images:string
+    available:boolean
 }
--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -9,30 +9,55 @@ import { Book } from './Book';
 export class BooksListComponent implements OnInit {
   horror_books:Book[]=[
     {
+      name:"Pet Sementary",
     type:"horror",
     images:"assets/images/Horror/images.jpg",
+    available:true
   },
   {
+    name:"Tales Horror",
     type:"horror",
-    images:"assets/images/Horror/tales_horror.jpg"
+    images:"assets/images/Horror/tales_horror.jpg",
+    available:true
   },
   {
+    name:"The Outsider",
     type:"horror",
-    images:"assets/images/Horror/images (1).jpg"
+    images:"assets/images/Horror/images (1).jpg",
+    available:true
+  },
+  {
+  name:"The Tokyo Zodiacs Murders",
+  type:"horror",
+  images:"assets/images/Horror/TheTokyoZOdiacMurders.jpg",
+  available:false
   }
-  ]
+  
+]
   fantasy_books:Book[]=[
   {
+    name:"Journey To The Center Of The Earth",
     type:"adventure",
-    images:"assets/images/Fantasy/journey.jpg"
+    images:"assets/images/Fantasy/journey.jpg",
+    available:true
   },
   {
+    name:"Moby Dick",
     type:"fantasy",
-    images:"assets/images/Fantasy/moby.jpg"
+    images:"assets/images/Fantasy/moby.jpg",
+    available:true
   },
   {
+    name:"Jurassic Park",
     type:"fantasy",
-    images:"assets/images/Fantasy/jp.jpg"
+    images:"assets/images/Fantasy/jp.jpg",
+    available:true
+  },
+  {
+    name:"Shadows On The Moon",
+    type:"fantasy",
+    images:"assets/images/Fantasy/Shadows-on-the-Moon.jpeg",
+    available:false
   }
 
 ]
